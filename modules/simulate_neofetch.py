c43e52fd082c003de1920f865fda67e37100f2f2
Oscar Rojas <rojas.oscar.duvan@gmail.com>
2025-01-09 17:08:59 -0500

    update

--- modules/simulate_neofetch.py
+++ modules/simulate_neofetch.py
@@ -8,32 +8,39 @@ import platform
 from modules.colors import BOLD, BLUE, RESET
 from modules.icons import icons
 
-
-
 def neofetch():
     uname = platform.uname()
     system = uname.system
     distro = ""
     version = ""
     
+    if system == "":
+        system = "Unknown"
+    
     if system == "Linux":
-      try:
-        with open("/etc/os-release") as f:
-          for line in f:
-            if line.startswith("NAME="):
-              distro = line.split("=")[1].strip().strip('"')
-            elif line.startswith("VERSION="):
-              version = line.split("=")[1].strip().strip('"')
-      except FileNotFoundError:
+        try:
+            with open("/etc/os-release") as f:
+                for line in f:
+                    if line.startswith("NAME="):
+                        distro = line.split("=")[1].strip().strip('"')
+                    elif line.startswith("VERSION="):
+                        version = line.split("=")[1].strip().strip('"')
+        except FileNotFoundError:
+            distro = "Linux"
+    
+    if "Linux" in system and not distro:
         distro = "Linux"
     
-    logo = icons.get(distro if distro else system, ["Unknown OS"])
+    if system == "Unknown":
+        logo = ["Sistema operativo no detectado"]
+    else:
+        logo = icons.get(distro if distro else system, icons["Unknown OS"])
     
     def siSystem():
-      if system == 'Windows':
-        return f"{BLUE}{system}{RESET}"
-      else:
-        return f"{system} ({distro})" if distro else f"{system}"
+        if system == 'Windows':
+            return f"{BLUE}{system}{RESET}"
+        else:
+            return f"{system} ({distro})" if distro else f"{system}"
     
     # Calcular el uptime en un formato legible
     boot_time_timestamp = psutil.boot_time()
@@ -44,22 +51,28 @@ def neofetch():
     minutes = (seconds % 3600) // 60
     readable_uptime = f"{days} days, {hours} hours, {minutes} mins" if days > 0 else f"{hours} hours, {minutes} mins"
     
+    users = psutil.users()
+    if users:
+        user_info = f"{BOLD}User:{RESET} {users[0].name}"
+    else:
+        user_info = f"{BOLD}User:{RESET} No users connected"
+    
     info = [
-      f"{BOLD}User:{RESET} {psutil.users()[0].name}",
-      f"{BOLD}System:{RESET} {siSystem()}",
-      f"{BOLD}Node Name:{RESET} {uname.node}",
-      f"{BOLD}Release:{RESET} {uname.release}",
-      f"{BOLD}Version:{RESET} {uname.version}",
-      f"{BOLD}Machine:{RESET} {uname.machine}",
-      f"{BOLD}Processor:{RESET} {uname.processor}",
-      f"{BOLD}RAM:{RESET} {round(psutil.virtual_memory().total / (1024.0 **3))} GB",
-      f"{BOLD}OS:{RESET} {distro} {version} on {system} {uname.machine}",
-      f"{BOLD}Kernel:{RESET} {uname.release}",
-      f"{BOLD}Uptime:{RESET} {readable_uptime}",
-      f"{BOLD}Packages:{RESET} {len(psutil.pids())} (dpkg)",
-      f"{BOLD}Shell:{RESET} {psutil.Process().name()}",
-      f"{BOLD}CPU Usage:{RESET} {psutil.cpu_percent()}%",
-      f"{BOLD}Memory Usage:{RESET} {psutil.virtual_memory().percent}%",
+        user_info,
+        f"{BOLD}System:{RESET} {siSystem()}",
+        f"{BOLD}Node Name:{RESET} {uname.node}",
+        f"{BOLD}Release:{RESET} {uname.release}",
+        f"{BOLD}Version:{RESET} {uname.version}",
+        f"{BOLD}Machine:{RESET} {uname.machine}",
+        f"{BOLD}Processor:{RESET} {uname.processor}",
+        f"{BOLD}RAM:{RESET} {round(psutil.virtual_memory().total / (1024.0 **3))} GB",
+        f"{BOLD}OS:{RESET} {distro} {version} on {system} {uname.machine}",
+        f"{BOLD}Kernel:{RESET} {uname.release}",
+        f"{BOLD}Uptime:{RESET} {readable_uptime}",
+        f"{BOLD}Packages:{RESET} {len(psutil.pids())} (dpkg)",
+        f"{BOLD}Shell:{RESET} {psutil.Process().name()}",
+        f"{BOLD}CPU Usage:{RESET} {psutil.cpu_percent()}%",
+        f"{BOLD}Memory Usage:{RESET} {psutil.virtual_memory().percent}%"
     ]
     
     # Definir la distancia fija arriba y abajo
@@ -72,11 +85,9 @@ def neofetch():
     
     max_lines = max(len(logo), len(info))
     for i in range(max_lines):
-      logo_line = logo[i] if i < len(logo) else ""
-      info_line = info[i] if i < len(info) else ""
-      print(f"{logo_line:<35} {info_line}")
-
-
+        logo_line = logo[i] if i < len(logo) else ""
+        info_line = info[i] if i < len(info) else ""
+        print(f"{logo_line:<35} {info_line}")
 
 if __name__ == "__main__":
-  neofetch()
+    neofetch()
